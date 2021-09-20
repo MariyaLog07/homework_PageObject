578eb356f274040ca53a308aceb03e04d8a34bfc
Mariya <90001486+MariyaLog07@users.noreply.github.com>
2021-09-20 21:09:32 +0300

    Delete TestData.java
    тестовые данные добавлены в один класс

--- src/test/java/ru/desnol/TestData.java
+++
@@ -1,9 +0,0 @@
-package ru.desnol;
-
-public class TestData {
-    public static String
-            firstName = "Mariya",
-            lastName = "Logutenko",
-            userEmail = "Test@gmail.com",
-            userNumber = "8900369852";
-}
